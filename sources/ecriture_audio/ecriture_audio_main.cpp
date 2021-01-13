899cac2d12420f03b666136d0ff2e13da2521b0c
20vernet <gabrielle.vernet@mines-paristech.fr>
2021-01-13 09:17:13 +0100

    adaptation ecriture audio en fonction

--- sources/ecriture_audio/ecriture_audio_main.cpp
+++ sources/ecriture_audio/ecriture_audio_main.cpp
@@ -1,11 +1,11 @@
 #include <parametre_audio.h>
 
 
-int main(){
+void ecriture_audio(double ti, double tf){
     FILE *fichier_wav = fopen("morse.wav", "wb");
     ecrire_entete_WAV(fichier_wav);
 
-    mon_signal(0.0, 5.0, 440.0, 1.0);
+    mon_signal(ti, tf, 440.0, 1.0);
     ecrire_donnees_normalisees_WAV(fichier_wav);
     fclose(fichier_wav);
 
